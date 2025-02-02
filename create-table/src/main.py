48329acdaa1a649c650f8fb23fcc6ff52aa0f3d0
icaroribeiro <icaroribeiro@hotmail.com>
2025-02-02 00:51:02 -0300

    Work in Progress

---
+++ create-table/src/main.py
@@ -0,0 +1,8 @@
+from worksteps.step_1.step_1 import run as run_step_1
+from worksteps.step_2.step_2 import run as run_step_2
+from worksteps.step_3.step_3 import run as run_step_3
+
+if __name__ == "__main__":
+    run_step_1()
+    run_step_2()
+    run_step_3()
